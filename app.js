faafa5e411f70f9a7be91f2702c14a27ebf28740
farooque-m <faqfaroooque597@gmail.com>
2021-10-04 00:06:57 +0530

    feat(admin): add new table and form for create and show product

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var adminRouter = require('./routes/admin');
 
 var hbs = require('express-handlebars');
 var app = express();
+var fileUpload = require('express-fileupload');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', usersRouter);
 app.use('/admin', adminRouter);
+app.use(fileUpload())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
